be36cb355504d740884cce11ef2064f3e3f9093f
Francois Lacoursiere <lacouf@gmail.com>
2023-03-16 18:12:11 -0400

    first commit

---
+++ src/test/java/ca/cal/tp3/Tp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.cal.tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
